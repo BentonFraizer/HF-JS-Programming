1d284d2272cf30edbc3d157ab01319ad6612a818
Yuri Lopukhin <FOL2203012@yandex.ru>
2020-12-22 05:42:58 +0300

    Start learning

---
+++ chapter_01/script.js
@@ -0,0 +1,56 @@
+// setTimeout(wakeUpUser, 5000);
+    // function wakeUpUser() {
+    //   alert("Ты собираешься оставаться на этой скучной странице вечно?");
+    // }
+    // var age = 14;
+    // var name = "Owen";
+    // if (age > 14) {
+    //   alert("Sorry this page is for kids only!");
+    // } else {
+    //   alert("Welcome " + name + "!");
+    // }
+    // var concatenate = "Dear" + " " + "Reader" + "!"
+    // console.log(concatenate);
+    // var age = 14;
+    // var howAge = age > 14;
+    // console.log("howAge is equal = " + howAge)
+    // var temp = 10;
+    // var Farenheit = (9 / 5) * temp + 32;
+    // console.log(Farenheit);
+
+    // color = "pink";
+    // console.log(color == "pink");
+
+    // var scoops = 5;
+    // while (scoops > 0) {
+    //   document.write("Следующий шарик!<br>");
+    //   scoops = scoops - 1;
+    //   document.write("Осталось шариков: " + scoops + "<br>");
+    // }
+    // document.write("Жизнь без мороженого невыносима!");
+    // var i = 0;
+    // var name = "Joe";
+
+    // while (i < 3) {
+    //   document.write("Happy Birthday to you.<br>");
+    //   i = i + 1;
+    // }
+    // document.write("Happy Birthday dear " + name + ",<br>");
+    // document.write("Happy Birthday to you.<br>");
+    // ==========================================================
+    // var word = "bottles";
+    // var count = 99;
+    // while (count > 0) {
+    //   console.log(count + " " + word + " of beer on the wall");
+    //   console.log(count + " " + word + " of beer,");
+    //   console.log("Take one down, pass it around,");
+    //   count = count - 1;
+    //   if (count > 0) {
+    //     console.log(count + " " + word + " of beer on the wall.");
+    //   } else {
+    //     console.log("No more " + word + " of beer on the wall.");
+    //   }
+
+    // }
+
+    //=============================================================
