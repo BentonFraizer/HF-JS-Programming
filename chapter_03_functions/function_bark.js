02e661ba67155a484af2a52e17b349aaaaaadfd7
Yuri Lopuhin <FOL2203012@yandex.ru>
2021-01-25 03:46:06 +0900

    Выполнено задание со стр. 121

--- chapter_03_functions/function_bark.js
+++ chapter_03_functions/function_bark.js
@@ -1,12 +1,34 @@
-function bark(name, weight) {
-  if (weight > 20) {
-    console.log(name + " says WOOF WOOF");
-  } else {
-    console.log(name + " says woof woof");
-  }
-}
+// function bark(name, weight) {
+//   if (weight > 20) {
+//     console.log(name + " says WOOF WOOF");
+//   } else {
+//     console.log(name + " says woof woof");
+//   }
+// }
+// bark("rover", 23);
+// bark("spot", 13);
+// bark("spike", 53);
+// bark("lady", 17);
+// bark("juno", 20);
+// bark("scottie", -1);
+// bark("dino", 0, 0);
+// bark("fido", "20");
+// bark("lady", 10);
+// bark("bruno", 21);
 
-bark("rover", 23);
-bark("spot", 13);
-bark("spike", 53);
-bark("lady", 17);
+//===================================================================================================
+// Задание со страницы 121 "Развлечение с магнитами" (выполнил самостоятельно)
+// function whatShallWear(temp) {
+//   if (temp < 60) {
+//     console.log("Wear a jacket");
+//   } else if (temp < 70) {
+//     console.log("Wear a sweater");
+//   } else {
+//     console.log("Wear t-shirt");
+//   }
+// }
+
+// whatShallWear(50);
+// whatShallWear(80);
+// whatShallWear(60);
+//===================================================================================================
